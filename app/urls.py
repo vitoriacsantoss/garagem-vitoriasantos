1df1c6350c7bce8013bbece987820201e5f9c0ed
vitoriacsantoss <santosviih360@gmail.com>
2024-04-04 11:58:57 -0300

    Criação da API Cor

--- app/urls.py
+++ app/urls.py
@@ -9,13 +9,14 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet , AcessorioViewSet , CategoriaViewSet
+from core.views import UserViewSet , AcessorioViewSet , CategoriaViewSet , CorViewSet
 
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
 router.register(r"acessorios",AcessorioViewSet)
 router.register(r"categorias" , CategoriaViewSet)
+router.register(r"cores", CorViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
